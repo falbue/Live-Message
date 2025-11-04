0d6491984d9fde2267647f9a124612462f549288
falbue love png <cyansair05@gmail.com>
2025-11-04 11:21:43 +0500

    Улучшена читаемость кода

--- app.py
+++ app.py
@@ -18,15 +18,17 @@ def chat(chat_id):
 def on_join(data):
     chat_id = data['chat_id']
     join_room(chat_id)
-    socketio.emit('receive_message', 
-                 {'text': 'Пользователь подключился!', 'sender_id': data['sender_id']}, 
-                 room=chat_id)
+    socketio.emit(
+        'receive_message', 
+        {'text': 'Пользователь подключился!', 'sender_id': data['sender_id']},
+        room=chat_id)
 
 @socketio.on('update_message')
 def handle_message(data):
-    socketio.emit('receive_message', 
-                 {'text': data['text'], 'sender_id': data['sender_id']}, 
-                 room=data['chat_id'])
+    socketio.emit(
+        'receive_message',
+        {'text': data['text'], 'sender_id': data['sender_id']},
+        room=data['chat_id'])
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    socketio.run(app, host='0.0.0.0', port=5001, debug=True)
