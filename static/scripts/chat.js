1d85302bfae08f642bc6ac760384ae9bfd2413b9
falbue love png <cyansair05@gmail.com>
2025-11-04 11:22:46 +0500

    Удалены лишние комментарии

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -10,16 +10,13 @@ function formatMessage(message) {
     return message.replace(/\n/g, '<br>');
 }
 
-// Подключаемся к чату
 socket.emit('join_chat', { chat_id: chatId, sender_id: senderId });
 
-// Отправка сообщений при изменении ввода
 inputMessage.addEventListener('input', () => {
     const messageText = inputMessage.value.trim() || '...';
     socket.emit('update_message', { chat_id: chatId, text: messageText, sender_id: senderId });
 });
 
-// Получение сообщений
 socket.on('receive_message', (data) => {
     if (data.sender_id !== senderId) {
         displayMessage.innerHTML = formatMessage(data.text);
